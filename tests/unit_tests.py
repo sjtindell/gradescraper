31fec9677cc2fe906a4a230176ef75aaa23edd8e
sjtindell <sjtindell@gmail.com>
2015-04-07 11:11:48 -0700

    unittests for calendar scrape returning correct number of activities, date formats

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -44,20 +44,67 @@ class ScrapeCalendarTest(unittest.TestCase):
 		self.url = 'http://simms-teach.com/cis90calendar.php'
 		self.response = requests.get(self.url)
 		self.soup = BeautifulSoup(self.response.text)
+		
+		self.calendar = get_calendar_data(self.url)
 	
 	def test_scrape_calendar_returns_table_rows(self):
 		expected_rows = self.soup.find_all('tr')[1:]
 		rows = scrape_calendar(self.url)
 		assert rows == expected_rows
-	
-	
-	# did we get all rows
+		assert len(rows) is 18
+		
+	# {lesson # {class date, activities in class, due today}}
+	def test_check_schedule_dict_lessons_are_ints(self):
+		for key in self.calendar.keys():
+			key = key.encode('ascii', 'ignore')
+			if key:
+				assert int(key)
 
-	# is the dict layout as expected
+	def test_check_schedule_dict_dates_format(self):
+		date_pattern = re.compile('^[0-9]+[/][0-9]+$')
+		for value in self.calendar.values():
+			assert date_pattern.match(value['date'])
+
+	def test_check_schedule_dict_number_of_activities(self):
+		quizzes, tests = 0, 0
+		for value in self.calendar.values():
+			assignment = value['in_class']
+			if 'Quiz' in assignment:
+				quizzes += 1
+			elif 'Test' in assignment:
+				tests += 1
+		assert quizzes is 10
+		assert tests is 3
+				
+		# due today
+		# right number of labs, forums
+	def test_check_schedule_dict_number_of_labs_and_forums(self):
+		check_strings = {
+			'Lab': 0, 
+			'posts': 0, 
+			'Project': 0, 
+			'Survey': 0, 
+			'X1': 0, 
+			'X2': 0
+		}
+		
+		for value in self.calendar.values():
+			due_today = value['due']
+			for assignment in due_today:
+				for string in check_strings.keys():
+					if string in assignment:
+							check_strings[string] += 1
 	
+		assert check_strings['Lab'] is 12
+		assert check_strings['posts'] is 4
+		assert check_strings['Project'] is 1
+		assert check_strings['Survey'] is 1
+		assert check_strings['X1'] is 1
+		assert check_strings['X2'] is 1
+		
+			
+
 	# are the dates as expected, spring vs fall
-	# is the point total expected
-	# are the dates handled right
 
 
 if __name__ == '__main__':
