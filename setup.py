0c593dd3ae76e6449c3cd5af7fc4f417bd95af22
sjtindell <sjtindell@gmail.com>
2015-04-07 04:55:17 -0700

    set up project skeleton, described initial calendar unittests

---
+++ setup.py
@@ -0,0 +1,18 @@
+try:
+	from setuptools import setup
+except ImportError:
+	from disutils.core import setup
+
+config = {
+	'description': 'Class grades/scheduler',
+	'author': 'sam tindell',
+	'download_url': '@github',
+	'author_email': 'sjtindell@gmail',
+	'version': '0.1',
+	'install_requires': ['nose'],
+	'packages': ['src'],
+	'scripts': [],
+	'name': 'planner'
+}
+
+setup(**config)
