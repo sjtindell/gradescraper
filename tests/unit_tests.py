92cde80bb225c832253b7e1dcc061f081b772eaa
sjtindell <sjtindell@gmail.com>
2015-04-07 14:01:40 -0700

    renamed get_cal_data format_cal_data, refactored unittests and scraper

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -4,7 +4,7 @@ import requests
 import time
 import unittest
 
-from src.scraper import scrape_calendar, get_calendar_data
+from src.scraper import scrape_calendar, format_calendar_data
 
 
 class CalendarPageTest(unittest.TestCase):
@@ -36,30 +36,31 @@ class CalendarPageTest(unittest.TestCase):
 		assert int(lesson_field)
 		date_field = table_data[1].string
 		date_pattern = re.compile('^[0-9]+[/][0-9]+$')
-		assert date_pattern.match(date_field)  # date
+		assert date_pattern.match(date_field)  # datie
 
 	
 class ScrapeCalendarTest(unittest.TestCase):
 
 	def setUp(self):
 		self.url = 'http://simms-teach.com/cis90calendar.php'
-		self.response = requests.get(self.url)
-		self.soup = BeautifulSoup(self.response.text)
-		
-		self.calendar = get_calendar_data(self.url)
+		# scraper.py.get_calenda_data
+		# returns dict {lesson: {date:v, in_class:v, due:v, }	
+		self.calendar = format_calendar_data(self.url)
 	
-	def test_scrape_calendar_returns_table_rows(self):
-		expected_rows = self.soup.find_all('tr')[1:]
+	def test_scrape_calendar_method_returns_table_rows(self):
+		response = requests.get(self.url)
+		soup = BeautifulSoup(response.text)
+		expected_rows = soup.find_all('tr')[1:]
+
 		rows = scrape_calendar(self.url)
+
 		assert rows == expected_rows
 		assert len(rows) is 18
 		
 	# {lesson # {class date, activities in class, due today}}
-	def test_check_schedule_dict_lessons_are_ints(self):
-		for key in self.calendar.keys():
-			key = key.encode('ascii', 'ignore')
-			if key:
-				assert int(key)
+	def test_check_schedule_dict_lessons_int_range(self):	
+		site_range = (int(key) for key in self.calendar.keys() if key)
+		assert sum(site_range) is sum(range(1, 16))
 
 	def test_check_schedule_dict_dates_format(self):
 		date_pattern = re.compile('^[0-9]+[/][0-9]+$')
@@ -69,7 +70,7 @@ class ScrapeCalendarTest(unittest.TestCase):
 	def test_check_schedule_dict_number_of_activities(self):
 		quizzes, tests = 0, 0
 		for value in self.calendar.values():
-			assignment = value['in_class']
+			assignment = value['activity']
 			if 'Quiz' in assignment:
 				quizzes += 1
 			elif 'Test' in assignment:
@@ -89,19 +90,14 @@ class ScrapeCalendarTest(unittest.TestCase):
 			'X2': 0
 		}
 		
-		for value in self.calendar.values():
-			due_today = value['due']
-			for assignment in due_today:
+		for assignment_list in self.calendar.values():
+			for assign in assignment_list['due']:
 				for string in check_strings.keys():
-					if string in assignment:
+					if string in assign:
 							check_strings[string] += 1
-	
-		assert check_strings['Lab'] is 12
-		assert check_strings['posts'] is 4
-		assert check_strings['Project'] is 1
-		assert check_strings['Survey'] is 1
-		assert check_strings['X1'] is 1
-		assert check_strings['X2'] is 1
+			
+		expected = (12, 4, 1, 1, 1, 1)
+		assert sum(expected) is sum(check_strings.values())
 			
 	def test_correct_dates_for_spring_and_fall(self):
 		today = time.strftime("%m/%d")
@@ -111,9 +107,4 @@ class ScrapeCalendarTest(unittest.TestCase):
 			if today < "09/01":
 				assert date < "09/01"
 			elif today > "09/01":
-				print 'it is fall'
 				assert date > "09/01"
-	
-
-if __name__ == '__main__':
-	unittest.main()
